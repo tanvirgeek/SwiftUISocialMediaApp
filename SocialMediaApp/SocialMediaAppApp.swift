4b0af11c842865679ab613a83da64aa99f46b7cb
tanvir <tanvirgeek@gmail.com>
2020-11-13 08:22:41 +0600

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by MD Tanvir Alam on 13/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
